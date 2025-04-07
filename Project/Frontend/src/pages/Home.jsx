6d786acfcefd944e7739a993d60ca6fa4a7ed7cc
Vinay reddy koukuntla <142045389+vny-rdy@users.noreply.github.com>
2025-04-08 02:39:02 +0530

    added Project frontend structure

---
+++ Project/Frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className='home'>
+        
+
+
+
+    </div>
+    <Footer/>
+    
+    </>
+  )
+}
+
+export default Home
